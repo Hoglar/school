3906b1ffaa561a7b5a8e5d0680ec01291aca042b
hoglar <martin.hoglar@gmail.com>
2024-12-20 20:15:40 +0100

    La inn så trekanter som ikke går bryter

--- innl_trekant/helper.py
+++ innl_trekant/helper.py
@@ -6,3 +6,34 @@ def sin_deg(x):
 
 # Kan lage en del funksjoner som finner sider og vinkler basert på hva jeg har
 
+# Lager en funksjon som sjekker par, får så tilbake "none", "both", "angle", "side"
+# Ikke planlagt hvordan den skal brukes, men tror det kan bli bra.
+
+# Tar list som parameter, list inneholder [vinkel, side]
+def check_pair(pair: list):
+    if not pair[0] and not pair[1]:
+        return None
+    elif pair[0] and not pair[1]:
+        return "angle"
+    elif not pair[0] and pair[1]:
+        return "side"
+    else:
+        return "both"
+    
+# Skal brukes i filter , tar pair, sjekker om jeg har både vinkel og side.
+def is_pair_done(pair: list) -> bool:
+    if pair[0] and pair[1]:
+        return True
+    else:
+        return False
+    
+# Lager et scoring system, trur at det kan hjelpe med å avgjøre om det kan løses eller ikke
+
+def triangle_score(pairs: list) -> int:
+    merged_pairs = [item for sublist in pairs for item in sublist]
+    score = 0
+    for x in merged_pairs:
+        if x:
+            score += 1
+    return score
+    
--- innl_trekant/main.py
+++ innl_trekant/main.py
@@ -1,5 +1,5 @@
 
-from helper import sin_deg
+from helper import sin_deg, check_pair, is_pair_done, triangle_score
 #Trekant 1:
 # Lage et program som tar vinkel A og vinkel B og lengden AB som input
     # To typer input, sjekke for parametere, eller ta input.
@@ -9,18 +9,15 @@ from helper import sin_deg
 
 # pair består av vinkel, og lengde på motstående side. 
 
-
-pair_a = [0, None]
-pair_b = [0, None]
-pair_c = [0, None]
-
+pair_a = [0, 0]
+pair_b = [0, 0]
+pair_c = [0, 0]
 
 def get_angle(name):
 
     #Lage en sjekke vinkel funksjon?
     leftover_angle = 180
     if not pair_a[0] and not pair_b[0] and not pair_c[0]:
-        print("Dont have any angles yett")
         pass
 
     elif bool(pair_a[0]) + bool(pair_b[0]) + bool(pair_c[0]) == 1:
@@ -47,7 +44,6 @@ def get_angle(name):
 
 # Tar her imot pair som argumenter, så den blir mere modulær.
 def get_side(side_name, a, b, c):
-    print(a, b , c)
     # Starter med å innhente data fra bruker. Sidene må være positive, men ingen maks grense.
     # om jeg har 2 sider, vil den tredje kunne bli regnet ut. Kan jobbe lit med å
     # Ta mindre input fra bruker, da jeg kan finne alt om jeg har alle vinkler
@@ -73,7 +69,6 @@ def get_side(side_name, a, b, c):
 
 print("Hei, du får nå noen sprørsmål angående vinklene og sidene i trekanten: ")
 
-
 pair_a[0] = get_angle("A")
 pair_b[0] = get_angle("B")
 pair_c[0] = get_angle("C")
@@ -98,4 +93,49 @@ pair_b[1] = get_side("AC", pair_a, pair_b, pair_c)
 
 #Nå kan vi kanskje få sider som ikke er gyldige, Kan feilsøke det litt, men trur jeg venter.
 
-print(pair_a, pair_b, pair_c)
+# Hvilke muligheter har jeg? vi dropper cos, sin og tan da vi ikke vet om trekanten er rett. 
+# Vi tar ikke høyde for at vi muligens har areal, så vi dropper og Arealsetingen.
+# Pytagoras utgår da det ikke er retvinklet.
+# Vi sitter igjen med sinussetningen og cosinus setningen. 
+
+#Sinus setningen
+# Vi trenger 2 vinkler og en side. eller en side og 2 vinkler.
+# Om vi har 2 sider og en vinkel er det skumelt, da vi kan få 2 trekanter. 
+# Tror det derfor er lurt å løse den til slutt.
+# Sin er bra, da jeg ikke trenger orden på hvem par som er hvem, alt er par mot par. 
+
+# Cosinus setningen
+# Er fin og ha om jeg har alle sidene.
+# Om det er tilfellet kan jeg egentlig bare finne alle vinklene med en gang. 
+# Data bryr seg ikke om at cosset er lang
+# Cosinus er og tryggere, er den ikke ?
+
+# Så hvor starter vi, først om vi kan løse i det hele tatt. om jeg ikke 
+
+# Må utelukke noen kasuser som ikke kan løses
+
+#Må jeg ha et par? Kan hvertfall finne par, i pairs vil indexen altid vite hvem side og vinkel
+pairs =[pair_a, pair_b, pair_c]
+
+while True:
+
+    done_pairs = [x for x in pairs if is_pair_done(x)]
+    pairs_to_fix = [x for x in pairs if not is_pair_done(x)]
+    print(pairs_to_fix)
+
+    if len(done_pairs) == 3:
+        print("Vi er ferdige", done_pairs)
+        break
+
+    # End program om vi har tilfeller som ikke kan løses
+    # Må ha mer en 1 side og en vinkel. Går heller ikke med 3 vinkler, 3 sider derimot er greit
+    if triangle_score(pairs) < 3:
+        print("Har ikke noe informasjon, får ikke løst trekanten")
+        break
+    elif check_pair(pair_a) and check_pair(pair_b) and check_pair(pair_c) == "angle":
+        print("Har bare vinkler, kan ikke løse")
+        break
+
+    # Kan nå starte med matte!
+    
+
