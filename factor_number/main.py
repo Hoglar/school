28330a3828c28181f440a4b026d02923cf2276ad
hoglar <martin.hoglar@gmail.com>
2024-12-25 11:05:45 +0100

    lager et program som faktoriserer

---
+++ factor_number/main.py
@@ -0,0 +1,6 @@
+# Lage et program som tar tall, så faktoriserer ned for så å gi tilbake primtallene 
+# i stigende rekkefølge.
+
+
+# Veldig enkelt trenger vi å ta inn et tall. 
+number = input("What number do you want to factorize? ")
