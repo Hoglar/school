d2facb5048ac2fb77a29db3347072e682af48536
hoglar <martin.hoglar@gmail.com>
2024-12-31 14:00:00 +0100

    Starter litt på boka

---
+++ fysikkBoka/ballslipp/main.py
@@ -0,0 +1,10 @@
+# Program ballposisjon.py
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.title("Test Plot")
+plt.show()
